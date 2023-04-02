f760b381d02ed3a4c531928800afd7aa1296ee1a
phamngocanh19127098 <npham4533@gmail.com>
2023-04-02 12:22:43 +0700

    create vpc, subnet, nic, route table, route table association, security group allow http, https, ssh. elastic ip depends internet gateway and launch ec2 instance in this vpc

--- main.tf
+++ main.tf
@@ -13,74 +13,137 @@ provider "aws" {
 }
 
 
-resource "aws_security_group_rule" "allow_ssh" {
-  type = "ingress"
-  security_group_id = "${data.aws_security_group.example.id}"
-  from_port = 22
-  to_port = 22
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+# # 1. Create vpc
 
-
-resource "aws_vpc" "public" {
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "name" {
-   cidr_block = "10.0.1.0/24"
-   vpc_id = aws_vpc.public.id
-
-}
-
-data "aws_security_group" "example" {
-  vpc_id      = aws_vpc.public.id
-}
-
-
-resource "aws_internet_gateway" "gateway" {
-  vpc_id = aws_vpc.public.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.public.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gateway.id
-  }
-
   tags = {
-    Name = "public"
+    "Name" = "production"
   }
+} 
+
+# # 2. Create Internet Gateway
+
+resource "aws_internet_gateway"  "gw" {
+    vpc_id = aws_vpc.prod-vpc.id
+    
+    tags = {
+      "Name" = "prod-gw"
+    }
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.name.id
-  route_table_id = aws_route_table.public.id
+# # 3. Create Custom Route Table
 
+resource "aws_route_table" "prod-route-table" {
+    vpc_id = aws_vpc.prod-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id 
+    }
+
+    route {
+        ipv6_cidr_block = "::/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
 }
 
-resource "aws_instance" "ec2" {
-  ami = "ami-0e2e292a9c4fb2f29"
-  instance_type = "t2.micro"
-  key_name      = "my_payment"
-  subnet_id     = aws_subnet.name.id
-  associate_public_ip_address = true
-  security_groups = [data.aws_security_group.example.id]
+# # 4. Create a Subnet
+resource "aws_subnet" "prod-subnet-1" {
+    vpc_id = aws_vpc.prod-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ap-southeast-1a"
 
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("/home/anhpham/Downloads/my_payment.pem")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum -y update",
-      "sudo yum -y install httpd",
-      "sudo systemctl start httpd",
-      "sudo systemctl enable httpd"
-    ]
-  }
+    tags = {
+      "Name" = "prod-subnet"
+    }
 }
+
+# # 5 Associate subnet with route table
+
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.prod-subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# # 6 Create a security group
+resource "aws_security_group" "allow_web" {
+    name = "allow_web_traffic"    
+    description = "Allow web inbound traffic"
+    vpc_id = aws_vpc.prod-vpc.id
+
+    ingress {
+        description = "HTTPS traffic"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP traffic"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "SSH traffic"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+    tags = {
+      Name = "Allow web"
+    }
+}
+
+# #7 Create a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "web-server-nic" {
+    subnet_id = aws_subnet.prod-subnet-1.id
+    private_ips = ["10.0.1.50"]
+    security_groups = [aws_security_group.allow_web.id]
+}
+
+# #8 Assign an elastic IP to the network interface created in step 7
+resource "aws_eip" "one" {
+  vpc = true
+  network_interface = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
+}
+
+# # 9 Create an Ubuntu Server and install/ enable apache2
+resource "aws_instance" "my-ec2-instance" {
+    ami = "ami-0a72af05d27b49ccb"
+    instance_type = "t2.micro"
+    key_name = "my_payment"
+  
+    network_interface {
+        device_index =  0
+        network_interface_id = aws_network_interface.web-server-nic.id
+    }
+  
+    user_data = <<-EOF
+        #!/bin/bash
+        sudo apt update -y
+        sudo apt install apache2 -y
+        sudo systemctl start apache2
+        sudo bash -c 'echo your verify web server > /var/www/html/index.html'
+        EOF
+
+    tags = {
+        Name = "MEME"
+    }
+}
+
